a1e6be34ef3383766c1c547257eb17491e6368c4
codingmate <hsykys0728@gmail.com>
2023-02-26 02:21:25 +0900

    using jpa auditing ( createdAt, modifiedAt )

--- src/main/kotlin/com/anyject/lottery/lotteryserver/Application.kt
+++ src/main/kotlin/com/anyject/lottery/lotteryserver/Application.kt
@@ -2,10 +2,12 @@ package com.anyject.lottery.lotteryserver
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
+@EnableJpaAuditing
 @SpringBootApplication
-class LotteryApplication
+class Application
 
 fun main(args: Array<String>) {
-    runApplication<LotteryApplication>(*args)
+    runApplication<Application>(*args)
 }
