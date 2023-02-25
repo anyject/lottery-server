d5a1d3089fdcf6a120ac0e3eaa5e248ac3e5be08
codingmate <hsykys0728@gmail.com>
2023-02-26 02:23:47 +0900

    create LotteryGenerator

---
+++ src/main/kotlin/com/anyject/lottery/lotteryserver/api/domain/lotto/LotteryGenerator.kt
@@ -0,0 +1,18 @@
+package com.anyject.lottery.lotteryserver.api.domain.lotto
+
+import kotlin.random.Random
+
+class LotteryGenerator {
+    fun generateLottoSet(): Set<Int>{
+
+        val lotterySet = HashSet<Int>()
+        while( lotterySet.size < 7 )
+            lotterySet.add(Random.nextInt(1, 46))
+
+        return lotterySet
+    }
+}
+
+fun main() {
+    println(LotteryGenerator().generateLottoSet())
+}
