3f59348582009b4d0ad7b3a90a2fcaefd3577fb5
codingmate <hsykys0728@gmail.com>
2023-02-26 02:24:18 +0900

    rename lotto -> lottery

--- src/main/kotlin/com/anyject/lottery/lotteryserver/api/web/LotteryController.kt
+++ src/main/kotlin/com/anyject/lottery/lotteryserver/api/web/LotteryController.kt
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/lotto")
-class LottoController{
+class LotteryController{
     @GetMapping("")
     fun lotto(): String {
 
--- src/test/kotlin/com/anyject/lottery/lotteryserver/api/web/LotteryControllerTest.kt
+++ src/test/kotlin/com/anyject/lottery/lotteryserver/api/web/LotteryControllerTest.kt
@@ -1,25 +1,35 @@
 package com.anyject.lottery.lotteryserver.api.web
 
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.springframework.beans.factory.annotation.Autowired
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.Mock
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.test.context.junit.jupiter.SpringExtension
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers
+import org.springframework.test.web.servlet.setup.MockMvcBuilders
 
-@WebMvcTest(LottoController::class)
-class LottoControllerTest
-    @Autowired constructor (
-        val mvc: MockMvc
-            ){
+@ExtendWith(SpringExtension::class)
+@WebMvcTest(LotteryController::class)
+class LotteryControllerTest {
+    private lateinit var mvc: MockMvc
+
+    @Mock
+    private lateinit var lottoController: LotteryController
+
     private val baseUrl = "/lotto"
+
+    @BeforeEach
+    fun setUp() {
+        mvc = MockMvcBuilders.standaloneSetup(lottoController).build()
+    }
 
     @Test
     fun `lotto api status test`() {
         lottoApiStatusTest(baseUrl)
-//        mvc.perform(MockMvcRequestBuilders.get(baseUrl))
-//            .andExpect(MockMvcResultMatchers.status().isOk)
-            //.andExpect(MockMvcResultMatchers.content().string(""))
+        //.andExpect(MockMvcResultMatchers.content().string(""))
     }
     private fun lottoApiStatusTest(uri: String) {
         mvc.perform(MockMvcRequestBuilders.get(uri)).andExpect(MockMvcResultMatchers.status().isOk)
