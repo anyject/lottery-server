a4bfdcfe44191d3a984a28cfdeec632bb0dcfb90
codingmate <hsykys0728@gmail.com>
2023-02-28 23:58:38 +0900

    move @EnableJpaAuditing) Application.kt -> JpaAuditingConfiguration.ktdi

--- src/test/kotlin/com/anyject/lottery/lotteryserver/api/web/LotteryControllerTest.kt
+++ src/test/kotlin/com/anyject/lottery/lotteryserver/api/web/LotteryControllerTest.kt
@@ -2,37 +2,33 @@ package com.anyject.lottery.lotteryserver.api.web
 
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.Mock
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
-import org.springframework.test.context.junit.jupiter.SpringExtension
+import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers
 import org.springframework.test.web.servlet.setup.MockMvcBuilders
-
-@ExtendWith(SpringExtension::class)
+//@MockBean(JpaMetamodelMappingContext::class)
 @WebMvcTest(LotteryController::class)
 class LotteryControllerTest {
     private lateinit var mvc: MockMvc
 
     @Mock
-    private lateinit var lottoController: LotteryController
+    private lateinit var lotteryController: LotteryController
 
     private val baseUrl = "/lotto"
 
     @BeforeEach
     fun setUp() {
-        mvc = MockMvcBuilders.standaloneSetup(lottoController).build()
+        mvc = MockMvcBuilders.standaloneSetup(lotteryController).build()
     }
 
     @Test
     fun `lotto api status test`() {
-        lottoApiStatusTest(baseUrl)
+        mvc.perform(MockMvcRequestBuilders.get(baseUrl)).andExpect(MockMvcResultMatchers.status().isOk)
         //.andExpect(MockMvcResultMatchers.content().string(""))
-    }
-    private fun lottoApiStatusTest(uri: String) {
-        mvc.perform(MockMvcRequestBuilders.get(uri)).andExpect(MockMvcResultMatchers.status().isOk)
     }
 }
 
