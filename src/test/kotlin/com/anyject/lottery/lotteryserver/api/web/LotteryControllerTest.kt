8554538a72c8812e203cfdcb6ebd5c17cf7b0b96
codingmate <hsykys0728@gmail.com>
2023-02-28 23:59:04 +0900

    불 필요 코드 삭제

--- src/test/kotlin/com/anyject/lottery/lotteryserver/api/web/LotteryControllerTest.kt
+++ src/test/kotlin/com/anyject/lottery/lotteryserver/api/web/LotteryControllerTest.kt
@@ -4,8 +4,6 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.mockito.Mock
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
-import org.springframework.boot.test.mock.mockito.MockBean
-import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers
